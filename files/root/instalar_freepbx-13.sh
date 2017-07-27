8f9375b44a2132f0bc1cbbba9990864b7f3eaba1
Antonio Alisio de Meneses Cordeiro <alisio.cordeiro@serpro.gov.br>
2017-07-27 08:41:47 -0300

    Criar regra para acesso web via porta 443

--- files/root/instalar_freepbx-13.sh
+++ files/root/instalar_freepbx-13.sh
@@ -9,6 +9,7 @@ sed -i 's/AllowOverride None/AllowOverride All/' /etc/httpd/conf/httpd.conf
 systemctl restart httpd.service
 pear install Console_Getopt
 firewall-cmd --zone=public --add-port=80/tcp --permanent
+firewall-cmd --zone=public --add-port=443/tcp --permanent
 firewall-cmd --reload
 cd /usr/src
 wget http://mirror.freepbx.org/modules/packages/freepbx/freepbx-13.0-latest.tgz
